b6185866da5d74b7addeaf11d100eca4305254eb
DKroco <vaness.bar@hotmail.fr>
2016-05-24 11:46:52 +0200

    add subject form

---
+++ public/subjectForm.php
@@ -0,0 +1,16 @@
+<!HTML>
+<html>
+	<head>
+		<title>Test ajout subject </title>
+	</head>
+
+	<body>
+		<form method="post" action="api/public/topics">
+			<label for="title">Titre</label>
+			<input type="text" name="title" id="title"/>
+			<input type="submit"/>
+		</form>
+	</body>
+
+
+</html>
