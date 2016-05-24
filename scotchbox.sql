8d95565a480abb7d9e0dba9a278c9fa99885ff2c
Luckypon <marion.vila.25@live.fr>
2016-05-24 10:20:42 +0200

    SQL update

--- scotchbox.sql
+++ scotchbox.sql
@@ -20,6 +20,11 @@ CREATE TABLE `COMMENTS` (
   CONSTRAINT `COMMENTS_ibfk_2` FOREIGN KEY (`id_answer`) REFERENCES `COMMENTS` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+INSERT INTO `COMMENTS` (`id`, `content`, `date`, `author`, `id_answer`, `id_post`) VALUES
+(1,	'Les barbies c\'est trop genial! On peut les demembrer !!!!!! :D',	'2016-05-24 08:07:59',	'le_nainfant',	NULL,	2),
+(2,	'Moi, je prefere les decapitations!',	'2016-05-24 08:08:08',	'Grrrrr_leVieux',	1,	2),
+(3,	'J\'ai jamais aime barbie elle pue !!! lol',	'2016-05-24 08:08:14',	'tonPere',	NULL,	2),
+(4,	'Je savais paaaas :O\r\n#IKnowNothing',	'2016-05-23 13:33:02',	'JonSnow',	NULL,	1);
 
 DROP TABLE IF EXISTS `POSTS`;
 CREATE TABLE `POSTS` (
@@ -35,6 +40,9 @@ CREATE TABLE `POSTS` (
   CONSTRAINT `POSTS_ibfk_1` FOREIGN KEY (`id_subject`) REFERENCES `SUBJECT` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+INSERT INTO `POSTS` (`id`, `title`, `content`, `image`, `date`, `id_subject`, `author`) VALUES
+(1,	'Episode 5, Season 5',	'Jon Snow dies.',	'http://www.thewrap.com/wp-content/uploads/2016/04/hbo-game-of-thrones-jon-snow.jpeg',	'2016-05-23 13:22:48',	1,	'VanessaTaMere'),
+(2,	'Barbie Girl Power',	'bla blaaaaaaaaaaaaa blaaa unicorn',	'https://s3-eu-west-1.amazonaws.com/spiked-online.com/images/barbie.jpg',	'0000-00-00 00:00:00',	2,	'theBestSinger');
 
 DROP TABLE IF EXISTS `SUBJECT`;
 CREATE TABLE `SUBJECT` (
@@ -55,6 +63,10 @@ CREATE TABLE `TAG` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `TAG` (`id`, `name`) VALUES
+(1,	'Chanson'),
+(3,	'LOL'),
+(2,	'Spoilers');
 
 DROP TABLE IF EXISTS `TAGGE`;
 CREATE TABLE `TAGGE` (
@@ -67,5 +79,8 @@ CREATE TABLE `TAGGE` (
   CONSTRAINT `TAGGE_ibfk_2` FOREIGN KEY (`id_tag`) REFERENCES `TAG` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+INSERT INTO `TAGGE` (`id_post`, `id_tag`) VALUES
+(1,	1),
+(1,	2);
 
--- 2016-05-24 08:14:00
+-- 2016-05-24 08:20:12
