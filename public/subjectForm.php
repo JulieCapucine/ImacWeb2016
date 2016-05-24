b069bfe85b0dffa42a56fbc14e3e9dfad57e7a4f
Luckypon <marion.vila.25@live.fr>
2016-05-24 15:51:41 +0200

    routes et form

--- public/subjectForm.php
+++ public/subjectForm.php
@@ -6,15 +6,27 @@
 
 	<body>
 		<form method="post" action="api/public/topics">
-			<label for="title">Titre</label>
-			<input type="text" name="title" id="title"/>
+			<label for="titre">Titre</label>
+			<input type="text" name="titre" id="titre"/>
 			<input type="submit"/>
 		</form>
 
 		<form method="post" action="api/public/topic/2">
-			<label for="title">Titre</label>
+			<label for="titre">Titre</label>
 			<input type="hidden" name="_METHOD" value="PUT"/>
-			<input type="text" name="title" id="title"/>
+			<input type="text" name="titre" id="titre"/>
+			<input type="submit"/>
+		</form>
+
+		<form method="post" action="api/public/post/2/tags">
+			<label for="titre">Rajout tags (avec virgule)</label>
+			<input type="text" name="nomTag" id="nomTag"/>
+			<input type="submit"/>
+		</form>
+		<h1> Suppression de tag pour le post 2 </h1>
+		<form method="post" action="api/public/post/2/tags">
+			<input type="hidden" name="_METHOD" value="DELETE"/>
+			<input type="text" name="toDeleteTag" id="toDeleteTag"/>
 			<input type="submit"/>
 		</form>
 	</body>
